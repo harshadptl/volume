67d1553ed192e8584d6bafbbbe45ab6d01c2823e
Harshad Patil <harshad0402@gmail.com>
2023-01-11 16:38:32 -0800

    Update Airport code check with Regex

--- types/types.go
+++ types/types.go
@@ -4,14 +4,37 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"regexp"
 )
 
+var pattern = regexp.MustCompile("^\"[A-Z]{3}\"$")
 
+//
 // AirportCode stores the three letter IATA code of an airport
+//
 type AirportCode string
 
+// While unmarshalling Airport code check if it is 3 character long surrounded by quotes
+func (d *AirportCode) UnmarshalJSON(data []byte) error {
+	if !pattern.Match(data) {
+		return ErrInvalidAirportCode
+	}
+	*d = AirportCode(data[1:4])
+	return nil
+}
+
+
+//
 // Flight is a two element array of Source and Destination airports
+//
 type Flight [2]AirportCode
+
+func (fl *Flight) Data() map[string]string {
+	return map[string]string{
+		"src": string(fl[0]),
+		"dest": string(fl[1]),
+	}
+}
 
 // FlightsData is an unordered list of flights in the itinerary of a person
 type FlightsData []Flight
@@ -25,12 +48,3 @@ func ParseFlightsData(r io.Reader) (*FlightsData, error) {
 	return &f, nil
 }
 
-// While unmarshalling Airport code check if it is 3 character long surrounded by quotes
-func (d *AirportCode) UnmarshalJSON(data []byte) error {
-	fmt.Println(data)
-	if len(data) != 5 || data[0]!=34 || data[4]!=34 {
-		return ErrInvalidAirportCode
-	}
-	*d = AirportCode(data[1:4])
-	return nil
-}
